32d100e2e635d7158c1f84a2d83a8c0d29c561cf
Cora <17833654450@163.com>
2026-02-02 18:06:03 +0800

    rewrite cmake files and improve compile effect

---
+++ cmake/common.cmake
@@ -0,0 +1,13 @@
+# C standard
+set(C_Standard 11)
+set(C_Standard_REQUIRED ON)
+set(C_EXTENSIONS OFF)
+
+# C++ standard
+set(CXX_STANDARD 20)
+set(CXX_STANDARD_REQUIRED ON)
+set(CXX_EXTENSIONS OFF)
+
+# Project settings
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+set(LIBRARY_OUTPUT_PATH ${CMAKE_CURRENT_SOURCE_DIR}/${BIN_FOLDER}/bin)
